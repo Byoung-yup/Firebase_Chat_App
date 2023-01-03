a5d20368c868f490015351f9d6d89799f7132910
Dev_Ozzy <20170926@vision.hoseo.edu>
2023-01-03 12:28:52 +0900

    Database Set Up

--- Firebase_Chat_App/Controllers/Login/LoginViewController.swift
+++ Firebase_Chat_App/Controllers/Login/LoginViewController.swift
@@ -109,7 +109,8 @@ class LoginViewController: UIViewController {
         }
         
         // Firebase Log In
-        FirebaseAuth.Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+        FirebaseAuth.Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
+            guard let self = self else { return }
             guard let result = authResult, error == nil else {
                 print("Failed to log in user with email: \(email)")
                 return
@@ -117,6 +118,7 @@ class LoginViewController: UIViewController {
             
             let user = result.user
             print("Logged In User: \(user)")
+            self.navigationController?.dismiss(animated: true)
         }
     }
     
